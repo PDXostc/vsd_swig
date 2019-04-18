e7bfc448c8e7149e23947b9e73fa251a72fac586
Magnus Feuer <mfeuer1@jaguarlandrover.com>
2019-04-18 10:55:28 -0700

    Added sample signal CSV file. example code now checks that signal file is successfully loaded.

--- vsd_pub_example.py
+++ vsd_pub_example.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 # Test python client to exercise DSTC.
 import vsd
-
+import sys
 
 
 if __name__ == "__main__":
     ctx = vsd.create_context()
-    vsd.load_from_file(ctx, "../vss_rel_2.0.0-alpha+005.csv")
+    if vsd.load_from_file(ctx, "./vss_rel_2.0.0-alpha+005.csv") != 0:
+        print("Could not load vss_rel_2.0.0-alpha+005.csv")
+        sys.exit(255)
+
     vsd.process_events(300000)
 
     sig = vsd.signal(ctx, "Vehicle.Drivetrain.Transmission.Gear")
--- vsd_sub_example.py
+++ vsd_sub_example.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 # Test python client to exercise DSTC.
 import vsd
+import sys
+
 ctx = None
 def process_signal(signal, path, value):
     print("Sub {}:{} - {}".format(signal, path, value))
@@ -8,7 +10,9 @@ def process_signal(signal, path, value):
 if __name__ == "__main__":
     ctx = vsd.create_context()
     vsd.set_callback(ctx, process_signal)
-    vsd.load_from_file(ctx, "../vss_rel_2.0.0-alpha+005.csv")
+    if vsd.load_from_file(ctx, "./vss_rel_2.0.0-alpha+005.csv") != 0:
+        print("Could not load vss_rel_2.0.0-alpha+005.csv")
+        sys.exit(255)
     sig = vsd.signal(ctx, "Vehicle.Drivetrain.Transmission");
     vsd.subscribe(ctx, sig)
     vsd.process_events(-1)
